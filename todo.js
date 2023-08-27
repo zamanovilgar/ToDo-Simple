1c7bd4bfb46107c06e25d0ee994c233c283392e6
zamanovilgar <zamanovilgar@gmail.com>
2023-08-27 11:31:49 +0400

    todo-simple

---
+++ todo.js
@@ -0,0 +1,27 @@
+const todoObj = {
+  name: "TodoApp",
+  data: [],
+  complete: [],
+
+  addTodo: function (text) {
+    this.data.push(text);
+  },
+  completedTodo: function (todoIndex) {
+    this.complete.push(this.data[todoIndex]);
+    const newData = this.data.filter((item, index) => index !== todoIndex);
+    this.data = newData;
+  },
+  renderTodo: function (listEl) {
+    listEl.innerHTML = this.data.map((item) => `<div>- ${item}</div>`).join("");
+  },
+};
+const todoInput = document.querySelector("#todoInput");
+const todoSend = document.querySelector("#todoSend");
+const todoList = document.querySelector("#todoList");
+
+todoSend.addEventListener("click", function () {
+  const value = todoInput.value;
+  todoObj.addTodo(value);
+
+  todoObj.renderTodo(todoList);
+});
